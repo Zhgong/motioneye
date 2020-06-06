d20b4f1e67840e0e6af97efbfecfbabedfb5d48f
seangongz <sean.gongz@gmail.com>
2020-06-06 18:17:59 +0200

    add feedback from server

--- send_bot/send_message.py
+++ send_bot/send_message.py
@@ -12,7 +12,8 @@ auth = HTTPBasicAuth(MESSAGE_TOKEN, "")
 def send(message:str)->None:
     """ Send message via telegram bot"""
     client = requests.post(f"https://{SERVER_IP}:{PORT}/api/v1/send", json={"message":str(message)}, auth=auth, verify=False)
+    return client.text
 
 
 if __name__ == "__main__":
-    send("Just a test")
+    print(send("Just a test"))
