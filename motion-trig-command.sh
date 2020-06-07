6fa6198842e805e09b4f0535295947cf6dfb2fd0
pi motioneye <pi@motioneye.xx>
2020-06-08 00:37:06 +0200

    use curl to send message

--- motion-trig-command.sh
+++ motion-trig-command.sh
@@ -5,13 +5,4 @@ echo "----- Motion -------" >> ${LOGFILE}
 
 source $(dirname $0)/.env-bash # use this file to transfer enviroment variables otherwise it will not work if this script was called by system
 
-echo PWD:$(pwd)
-
-date >> ${LOGFILE}
-echo Running as $USER >> ${LOGFILE}
-whoami  >> ${LOGFILE}
-echo ENV: >> ${LOGFILE}
-env >> ${LOGFILE}
-export PATH=$PATH:"/home/pi/.local/bin"
-echo $PATH >> ${LOGFILE}
-send-bot "[Mailbox] [有信件]" >> ${LOGFILE} 2>&1
+curl -X POST -H "Content-Type: application/json" -u "${MESSAGE_TOKEN}:" -d '{"message":"[Mailbox] [有信件]"}' -k https://${SERVER_IP}:${PORT}/api/v1/send >> ${LOGFILE} 2>&1
