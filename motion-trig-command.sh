85623a970a3924ee6dc159ac39df21422a8ba413
pi motioneye <pi@motioneye.xx>
2020-06-10 23:50:45 +0200

    add redirect for all command in script

--- motion-trig-command.sh
+++ motion-trig-command.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 LOGFILE=$0.log
-echo "----- Motion -------" >> ${LOGFILE}
-date >> ${LOGFILE}
+exec 1>>${LOGFILE}
+exec 2>&1
+
+echo "----- Motion -------"
+date
 
 source $(dirname $0)/.env-bash # use this file to transfer enviroment variables otherwise it will not work if this script was called by system
 
-curl -X POST -H "Content-Type: application/json" -u "${MESSAGE_TOKEN}:" -d '{"message":"[Mailbox] [有信件]"}' -k https://${SERVER_IP}:${PORT}/api/v1/send >> ${LOGFILE} 2>&1
+curl -X POST -H "Content-Type: application/json" -u "${MESSAGE_TOKEN}:" -d '{"message":"[Mailbox] [有信件]"}' -k https://${SERVER_IP}:${PORT}/api/v1/send
